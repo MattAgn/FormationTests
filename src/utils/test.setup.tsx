dce9eb7beba661550b6369c23dff851a0b02fbe6
MattAgn <matthieua@bam.tech>
2019-08-28 07:49:00 +0200

    :wrench: (test) hide act warning more properly

--- src/utils/test.setup.tsx
+++ src/utils/test.setup.tsx
@@ -2,9 +2,10 @@
  * Suppress React 16.8 act() warnings globally.
  * Waiting for react-native to support react 16.9
  */
-export const consoleError = console.error;
-jest.spyOn(console, 'error').mockImplementation((...args) => {
-  if (!args[0].includes('Warning: An update to %s inside a test was not wrapped in act')) {
-    consoleError(...args);
+export const originalError = console.error;
+console.error = (...args) => {
+  if (/Warning.*not wrapped in act/.test(args[0])) {
+    return;
   }
-});
+  originalError.call(console, ...args);
+};
