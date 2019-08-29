6fe82a91ab0a1c6a8a77594a3f5a9a210721dd7f
MattAgn <matthieua@bam.tech>
2019-08-29 23:04:06 +0200

    :art: clean tests for demo

--- src/basic-version/pages/Home/__tests__/Home.test.tsx
+++ src/basic-version/pages/Home/__tests__/Home.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'jest-styled-components';
 import { fireEvent, waitForElement, render } from 'react-native-testing-library';
 import fetchMock from 'fetch-mock';
 
@@ -11,35 +10,6 @@ describe('[Page] Home', () => {
   beforeEach(() => {
     fetchMock.reset();
   });
-  it('should display succesful message on successful subscribe', async () => {
-    // Setup
-    fetchMock.post(EMAIL_API_ENDPOINT, 200);
-    const page = render(<Home />);
-    // What the user sees
-    const EmailInput = page.getByPlaceholder(wording.emailPlaceholder);
-    const ValidateButton = page.getByText(wording.validateEmail);
-    // What the user does
-    fireEvent.changeText(EmailInput, 'hello@bam.co');
-    fireEvent.press(ValidateButton);
-    // What feedback the user should expect
-    const SuccessMessage = await waitForElement(() =>
-      page.queryByText(wording.subscriptionSuccessful)
-    );
-    expect(SuccessMessage).toBeTruthy();
-  });
 
-  it('should display error message on failed subscribe', async () => {
-    // Setup
-    fetchMock.post(EMAIL_API_ENDPOINT, 400);
-    const page = render(<Home />);
-    // What the user sees
-    const EmailInput = page.getByPlaceholder(wording.emailPlaceholder);
-    const ValidateButton = page.getByText(wording.validateEmail);
-    // What the user does
-    fireEvent.changeText(EmailInput, 'hellobam.co');
-    fireEvent.press(ValidateButton);
-    // What feedback the user should expect
-    const ErrorMessage = await waitForElement(() => page.queryByText(wording.basicError));
-    expect(ErrorMessage).toBeTruthy();
-  });
+  it('should display succesful message on successful subscribe', async () => {});
 });
--- src/complex-version/pages/__tests__/Home.test.tsx
+++ src/complex-version/pages/__tests__/Home.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'jest-styled-components';
-import { fireEvent, waitForElement } from 'react-native-testing-library';
+import { fireEvent, waitForElement, flushMicrotasksQueue } from 'react-native-testing-library';
 import fetchMock from 'fetch-mock';
 
 import { Home } from '../Home';
@@ -13,35 +12,5 @@ describe('[Page] Home', () => {
     fetchMock.reset();
   });
 
-  it('should display succesful message on successful subscribe', async () => {
-    // Setup
-    fetchMock.post(EMAIL_API_ENDPOINT, 200);
-    const page = renderPage(<Home />);
-    // What the user sees
-    const EmailInput = page.getByPlaceholder(wording.emailPlaceholder);
-    const ValidateButton = page.getByText(wording.validateEmail);
-    // What the user does
-    fireEvent.changeText(EmailInput, 'hello@bam.co');
-    fireEvent.press(ValidateButton);
-    // What feedback the user should expect
-    const SuccessMessage = await waitForElement(() =>
-      page.queryByText(wording.subscriptionSuccessful)
-    );
-    expect(SuccessMessage).toBeTruthy();
-  });
-
-  it('should display error message on failed subscribe', async () => {
-    // Setup
-    fetchMock.post(EMAIL_API_ENDPOINT, 400);
-    const page = renderPage(<Home />);
-    // What the user sees
-    const EmailInput = page.getByPlaceholder(wording.emailPlaceholder);
-    const ValidateButton = page.getByText(wording.validateEmail);
-    // What the user does
-    fireEvent.changeText(EmailInput, 'hellobam.co');
-    fireEvent.press(ValidateButton);
-    // What feedback the user should expect
-    const ErrorMessage = await waitForElement(() => page.queryByText(wording.basicError));
-    expect(ErrorMessage).toBeTruthy();
-  });
+  it('should display succesful message on successful subscribe', async () => {});
 });
